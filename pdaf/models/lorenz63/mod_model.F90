b289563018e17dc32ff9d7b3631ec9c7b31d9177
Qi Tang <tang1@jsfl01.jusuf>
2023-08-06 19:07:14 +0200

    pdaf source code

---
+++ pdaf/models/lorenz63/mod_model.F90
@@ -0,0 +1,32 @@
+!$Id: mod_model.F90 178 2019-07-03 15:07:04Z lnerger $
+!BOP
+!
+! !MODULE:
+MODULE mod_model
+
+! !DESCRIPTION:
+! This module provides shared variables for the dummy model.
+!
+! !REVISION HISTORY:
+! 2004-11 - Lars Nerger - Initial code
+! Later revisions - see svn log
+!
+! !USES:
+  IMPLICIT NONE
+  SAVE
+
+! !PUBLIC DATA MEMBERS:  
+!    ! Control model run - available as command line options
+  INTEGER :: dim_state           ! Model state dimension
+  INTEGER :: step_null           ! Initial time step of assimilation
+
+!    ! Other variables - _NOT_ available as command line options!
+  INTEGER :: step_final          ! Final time step
+  REAL    :: dt                  ! Time step size
+  REAL, ALLOCATABLE :: x(:)      ! Array holding model field
+  REAL    :: gamma               ! Model parameter
+  REAL    :: rho                 ! Model parameter
+  REAL    :: beta                ! Model parameter
+!EOP
+
+END MODULE mod_model
